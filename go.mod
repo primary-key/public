81f288a404c249b8b8e71e174c7898af8a577535
Prokhor Fedulov <polkipylesos@gmail.com>
2024-04-07 15:55:52 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/primary-key/public
+
+go 1.21.1
+
+require github.com/primary-key/private v0.0.0-20240407114917-e2a7c4f20ac5 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/primary-key/private"
+)
+
+func main() {
+	t := private.Type{}
+	fmt.Println("private repository has been loaded successfully")
+	fmt.Println(t)
+}
